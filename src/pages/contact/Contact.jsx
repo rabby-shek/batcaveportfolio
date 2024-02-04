c0e9abffa821c42430e2bf466ec8555995a3d30c
rabby-shek <rabbyshek1998@gmail.com>
2024-02-04 14:34:01 +0600

    initial push

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -3,7 +3,17 @@ import React from 'react';
 const Contact = () => {
   return (
     <section className='home-container'>
-      Contact
+      <div className="contact-container">
+        <h1>Contact</h1>
+      <div>
+        <div className='email'>rabbyshek1998@gmail.com</div>
+        <div className='phone'>+8801724707680</div>
+        <div className="location">Matuail, Jatrabari, Dhaka, Bangladesh</div>
+        <div>
+        <input type="submit" value="Send Email" />
+        </div>
+      </div>
+      </div>
     </section>
   )
 }
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,52 +1,74 @@
-import React from 'react';
-import HangMAN from '../../assets/images/hang-man-game.PNG'
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import HangMAN from "../../assets/images/hang-man-game.PNG";
+import RPOS from "../../assets/images/r-pos.PNG";
+import DOCRX from "../../assets/images/docrx.PNG";
+import { NavLink } from "react-router-dom";
 const Projects = () => {
   return (
-    <section className='home-container'>
+    <section className="home-container">
       <div className="projects">
         <h1>Projects</h1>
         <div className="project-container">
-      <div className="project-body">
-        {/* Repeat this structure for each project item */}
-        <div className="project-item">
-          <div className="project-image">
-            <img src={HangMAN} alt="project image" />
-          </div>
-          <div className="project-details">
-          Hangman is a classic word-guessing game that has entertained and challenged players for generations. 
-          <NavLink to="https://github.com/rabby-shek/hangman_game" target='_blank'><button className='resume'></button></NavLink>
+          <div className="project-body">
+            {/* Repeat this structure for each project item */}
+            <div className="project-item">
+              <div className="project-image">
+                <img src={HangMAN} alt="project image" />
+              </div>
+              <h1>Hang Man Game</h1>
+              <div className="project-details">
+                Hangman is a classic word-guessing game that has entertained and
+                challenged players for generat...
+                <div>
+                <NavLink
+                  to="https://github.com/rabby-shek/hangman_game"
+                  target="_blank"
+                >
+                  <button className="resume"></button>
+                </NavLink>
+                </div>
+              </div>
+            </div>
+            <div className="project-item">
+              <div className="project-image">
+                <img src={RPOS} alt="project image" />
+              </div>
+              <div className="project-details">
+                <h1>R-POS</h1>
+                R-POS is a comprehensive Restaurant Point-of-Sale System
+                designed to streamline order processing... 
+                <div>
+                <NavLink
+                  to="https://github.com/rabby-shek/project-minus-31"
+                  target="_blank"
+                >
+                  <button className="resume"></button>
+                </NavLink>
+                </div>
+              </div>
+            </div>
+            <div className="project-item">
+              <div className="project-image">
+                <img src={DOCRX} alt="project image" />
+              </div>
+              <div className="project-details">
+                <h1>DocRx</h1>
+                Cloud based Smart Prescription and  appointment management system...
+                <div>
+                <NavLink
+                  to="https://smartprescription.hishabkitab.com/"
+                  target="_blank"
+                >
+                  <button className="resume"></button>
+                </NavLink>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="project-item">
-          <div className="project-image">
-            <img src="" alt="project image" />
-          </div>
-          <div className="project-details">
-            Project details
-          </div>
-        </div>
-        <div className="project-item">
-          <div className="project-image">
-            <img src="" alt="project image" />
-          </div>
-          <div className="project-details">
-            Project details
-          </div>
-        </div>
-        <div className="project-item">
-          <div className="project-image">
-            <img src="" alt="project image" />
-          </div>
-          <div className="project-details">
-            Project details
-          </div>
-        </div>
-      </div>
-    </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Projects;
